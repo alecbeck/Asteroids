5d6897d390b20ba663b6fe9d1c02790483bc1590
alecbeck <alecbecklin@gmail.com>
2024-09-26 01:03:05 -0500

    Player Move and fixed roation bug

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
 
+
 		# in the player class
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -18,11 +19,10 @@ class Player(CircleShape):
 		c = self.position - forward * self.radius + right
 		return [a, b, c]
 	
+
 	def draw(self, screen):
 		pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
 
-	def rotate(self, dt):
-		self.rotation += PLAYER_TURN_SPEED + dt
 
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
@@ -31,3 +31,16 @@ class Player(CircleShape):
 			self.rotate(dt * -1)
 		if keys[pygame.K_d]:
 			self.rotate(dt)
+		if keys[pygame.K_w]:
+			self.move(dt)
+		if keys[pygame.K_s]:
+			self.move(dt * -1)
+	
+	
+	def rotate(self, dt):
+		self.rotation += PLAYER_TURN_SPEED * dt
+
+
+	def move(self, dt):
+		forward = pygame.Vector2(0, 1).rotate(self.rotation)
+		self.position += forward * PLAYER_SPEED * dt
